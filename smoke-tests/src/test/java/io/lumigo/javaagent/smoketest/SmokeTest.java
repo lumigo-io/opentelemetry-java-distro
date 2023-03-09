d5c53eed8471de7fef5213d36f9c9eed692861c1
almogbaku <almog.baku@gmail.com>
2023-03-09 19:00:13 +0200

    fix: unstable tests

--- smoke-tests/src/test/java/io/lumigo/javaagent/smoketest/SmokeTest.java
+++ smoke-tests/src/test/java/io/lumigo/javaagent/smoketest/SmokeTest.java
@@ -45,7 +45,6 @@ import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.MountableFile;
 
