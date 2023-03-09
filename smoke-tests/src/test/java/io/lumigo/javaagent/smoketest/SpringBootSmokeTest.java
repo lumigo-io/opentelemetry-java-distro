ab423e3b807dd0e4d11f9eee1397978af6e3101c
Almog Baku <almog.baku@gmail.com>
2023-03-09 17:56:09 +0200

    fix: export the spandump as json (#11)
    * fix: export the spandump as json
    
    * fix: fixes @mmanciop feedback
    
    * fix: @mmanciop feedback: silently fail when not path-like
    
    * fix: @mmanciop code review feedback
    
    * fix: @mmanciop code review feedback: add assertion
    
    * fix: lint on .gradle and FileLoggingSpanExporter
    
    ---------
    
    Co-authored-by: Michele Mancioppi <michelem@lumigo.io>

--- smoke-tests/src/test/java/io/lumigo/javaagent/smoketest/SpringBootSmokeTest.java
+++ smoke-tests/src/test/java/io/lumigo/javaagent/smoketest/SpringBootSmokeTest.java
@@ -20,6 +20,7 @@ package io.lumigo.javaagent.smoketest;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import okhttp3.Request;
@@ -37,8 +38,15 @@ class SpringBootSmokeTest extends SmokeTest {
   }
 
   @Test
+  public void testInvalidSpanDump() throws IOException, InterruptedException {
+    startTarget(8, Map.of("LUMIGO_DEBUG_SPANDUMP", "invalid"));
+
+    Assertions.assertTrue(target.getLogs().contains("Lumigo debug span dump is not a valid path"));
+  }
+
+  @Test
   public void springBootSmokeTestOnJDK() throws IOException, InterruptedException {
-    startTarget(8);
+    startTarget(8, null);
 
     // check if the debug log is printed
     Assertions.assertTrue(
@@ -66,8 +74,10 @@ class SpringBootSmokeTest extends SmokeTest {
         0, countResourcesByValue(traces, "telemetry.auto.version", currentAgentVersion));
     Assertions.assertNotEquals(0, countResourcesByValue(traces, "lumigo.distro.version", "dev"));
 
-    String spanLog = fetchSpanDumpFromTarget();
-    Assertions.assertNotEquals("", spanLog);
+    Collection<ExportTraceServiceRequest> dumpTraces = tracesFromSpanDump();
+    Assertions.assertEquals(1, countSpansByName(dumpTraces, "GET /greeting"));
+    Assertions.assertNotEquals(
+        0, countResourcesByValue(dumpTraces, "lumigo.distro.version", "dev"));
 
     stopTarget();
   }
