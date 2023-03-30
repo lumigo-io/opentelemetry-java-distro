4253989d1275f9294d3148151cfe70166e3d8bc3
almogbaku <almog.baku@gmail.com>
2023-03-30 14:20:49 +0300

    build: support multiple deployment in the same time

--- integration-tests/ecs/src/main.ts
+++ integration-tests/ecs/src/main.ts
@@ -6,8 +6,14 @@ const env = {
   region: process.env.CDK_DEFAULT_REGION,
 };
 
+// Allow to deploy multiple stacks in the same account
+let suffix = process.env.DEPLOYMENT_SUFFIX || '';
+if (suffix !== '') {
+  suffix = '-' + suffix;
+}
+
 const app = new App();
 
-new EcsFargateElbStack(app, 'lumigo-java-distro-itests-dev', { env });
+new EcsFargateElbStack(app, 'lumigo-java-distro-itests-dev'+ suffix, { env, tags: { 'lumigo:suffix': suffix } });
 
 app.synth();
