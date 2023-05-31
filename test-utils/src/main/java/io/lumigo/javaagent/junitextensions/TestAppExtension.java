b8f111c793cc4a7e012233e3d1e2c6a9753d1f0c
Ken Finnigan <ken@kenfinnigan.me>
2023-05-31 15:28:28 -0400

    chore: Change to only start smoke-test back end once (#34)

--- test-utils/src/main/java/io/lumigo/javaagent/junitextensions/TestAppExtension.java
+++ test-utils/src/main/java/io/lumigo/javaagent/junitextensions/TestAppExtension.java
@@ -28,10 +28,15 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
@@ -44,7 +49,8 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.MountableFile;
 
-public class TestAppExtension implements ParameterResolver, AfterEachCallback {
+public class TestAppExtension
+    implements ParameterResolver, BeforeAllCallback, AfterAllCallback, AfterEachCallback {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TestAppExtension.class);
 
@@ -57,6 +63,27 @@ public class TestAppExtension implements ParameterResolver, AfterEachCallback {
 
   private GenericContainer<?> backend;
   private GenericContainer<?> testApp;
+
+  protected static OkHttpClient client = clientBuilder().build();
+
+  /*
+   * If this is true, we will be running Docker in emulation, and it is
+   * *much* slower.
+   */
+  protected static final boolean IS_ARCH_AMD_64 = "amd64".equals(System.getProperty("os.arch"));
+
+  protected static final Duration BACKEND_CONTAINER_START_TIMEOUT =
+      Duration.ofMinutes(IS_ARCH_AMD_64 ? 3 : 15);
+  protected static final Duration APP_CONTAINER_START_TIMEOUT =
+      Duration.ofMinutes(IS_ARCH_AMD_64 ? 2 : 15);
+
+  static OkHttpClient.Builder clientBuilder() {
+    TimeUnit unit = TimeUnit.MINUTES;
+    return new OkHttpClient.Builder()
+        .connectTimeout(1, unit)
+        .writeTimeout(1, unit)
+        .readTimeout(1, unit);
+  }
 
   @Retention(RetentionPolicy.RUNTIME)
   @java.lang.annotation.Target(ElementType.PARAMETER)
@@ -85,13 +112,44 @@ public class TestAppExtension implements ParameterResolver, AfterEachCallback {
   }
 
   @Override
-  public void afterEach(ExtensionContext context) {
+  public void beforeAll(ExtensionContext context) throws Exception {
+    this.backend =
+        new GenericContainer<>(
+                "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-fake-backend:20221127.3559314891")
+            // .withStartupTimeout(BACKEND_CONTAINER_START_TIMEOUT)
+            .withExposedPorts(8080)
+            // .withEnv("JAVA_TOOL_OPTIONS", "-Xmx128m")
+            .waitingFor(Wait.forHttp("/health").forPort(8080))
+            .withNetwork(NETWORK)
+            .withNetworkAliases("backend")
+            .withLogConsumer(new Slf4jLogConsumer(LOGGER));
+    this.backend.start();
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) throws IOException {
+    client
+        .newCall(
+            new Request.Builder()
+                .url(String.format("http://localhost:%d/clear", backend.getMappedPort(8080)))
+                .build())
+        .execute()
+        .close();
+
     try {
       this.testApp.stop();
-      this.testApp = null;
     } finally {
+      this.testApp = null;
+    }
+  }
+
+  @Override
+  public void afterAll(ExtensionContext context) throws Exception {
+    try {
       this.backend.stop();
+    } finally {
       this.backend = null;
+      NETWORK.close();
     }
   }
 
@@ -107,32 +165,11 @@ public class TestAppExtension implements ParameterResolver, AfterEachCallback {
       ParameterContext parameterContext, ExtensionContext extensionContext)
       throws ParameterResolutionException {
 
-    /*
-     * If this is true, we will be running Docker in emulation, and it is
-     * *much* slower.
-     */
-    final boolean isArchAmd64 = "amd64".equals(System.getProperty("os.arch"));
-
-    final Duration backendContainerStartTimeout = Duration.ofMinutes(isArchAmd64 ? 3 : 15);
-    final Duration appContainerStartTimeout = Duration.ofMinutes(isArchAmd64 ? 2 : 15);
-
     final String jdkVersion =
         parameterContext
             .findAnnotation(Configuration.class)
             .map(Configuration::jdkVersion)
             .orElse("8");
-
-    this.backend =
-        new GenericContainer<>(
-                "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-fake-backend:20221127.3559314891")
-            .withStartupTimeout(backendContainerStartTimeout)
-            .withExposedPorts(8080)
-            .withEnv("JAVA_TOOL_OPTIONS", "-Xmx128m")
-            .waitingFor(Wait.forHttp("/health").forPort(8080))
-            .withNetwork(NETWORK)
-            .withNetworkAliases("backend")
-            .withLogConsumer(new Slf4jLogConsumer(LOGGER));
-    this.backend.start();
 
     final String testAppImage =
         "ghcr.io/open-telemetry/opentelemetry-java-instrumentation/smoke-test-spring-boot:jdk"
@@ -141,7 +178,7 @@ public class TestAppExtension implements ParameterResolver, AfterEachCallback {
 
     this.testApp =
         new GenericContainer<>(testAppImage)
-            .withStartupTimeout(appContainerStartTimeout)
+            .withStartupTimeout(APP_CONTAINER_START_TIMEOUT)
             .withExposedPorts(8080)
             .withNetwork(NETWORK)
             .withLogConsumer(new Slf4jLogConsumer(LOGGER))
