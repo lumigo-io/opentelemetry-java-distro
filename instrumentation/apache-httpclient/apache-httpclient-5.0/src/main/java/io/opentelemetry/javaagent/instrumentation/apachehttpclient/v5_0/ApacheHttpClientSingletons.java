40590646c2e5076f5ece4a178052655a1b7b2621
Ken Finnigan <ken@kenfinnigan.me>
2023-06-30 16:24:40 -0400

    Rebase onto OTeL 1.27.0
    - Moved classes back to the same package as the original instrumentation
    - Updated documentation on what we've changed from the original instrumentation

---
+++ instrumentation/apache-httpclient/apache-httpclient-5.0/src/main/java/io/opentelemetry/javaagent/instrumentation/apachehttpclient/v5_0/ApacheHttpClientSingletons.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.apachehttpclient.v5_0;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientAttributesExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientMetrics;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpSpanNameExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.http.HttpSpanStatusExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.net.PeerServiceAttributesExtractor;
+import io.opentelemetry.javaagent.bootstrap.internal.CommonConfig;
+import org.apache.hc.core5.http.HttpRequest;
+import org.apache.hc.core5.http.HttpResponse;
+
+public final class ApacheHttpClientSingletons {
+  private static final String INSTRUMENTATION_NAME = "io.opentelemetry.lumigo-apache-httpclient-5.0";
+
+  private static final Instrumenter<HttpRequest, HttpResponse> INSTRUMENTER;
+
+  static {
+    ApacheHttpClientHttpAttributesGetter httpAttributesGetter =
+        new ApacheHttpClientHttpAttributesGetter();
+    ApacheHttpClientNetAttributesGetter netAttributesGetter =
+        new ApacheHttpClientNetAttributesGetter();
+
+    INSTRUMENTER =
+        Instrumenter.<HttpRequest, HttpResponse>builder(
+                GlobalOpenTelemetry.get(),
+                INSTRUMENTATION_NAME,
+                HttpSpanNameExtractor.create(httpAttributesGetter))
+            .setSpanStatusExtractor(HttpSpanStatusExtractor.create(httpAttributesGetter))
+            .addAttributesExtractor(
+                HttpClientAttributesExtractor.builder(httpAttributesGetter, netAttributesGetter)
+                    .setCapturedRequestHeaders(CommonConfig.get().getClientRequestHeaders())
+                    .setCapturedResponseHeaders(CommonConfig.get().getClientResponseHeaders())
+                    .build())
+            .addAttributesExtractor(
+                PeerServiceAttributesExtractor.create(
+                    netAttributesGetter, CommonConfig.get().getPeerServiceMapping()))
+            // Custom HTTP payload extractor
+            .addAttributesExtractor(new HttpPayloadExtractor())
+            // Custom Context customizer for holding response payload
+            .addContextCustomizer((context, request, attributes) -> new ResponsePayloadBridge.Builder().init(context))
+            .addOperationMetrics(HttpClientMetrics.get())
+            .buildClientInstrumenter(HttpHeaderSetter.INSTANCE);
+  }
+
+  public static Instrumenter<HttpRequest, HttpResponse> instrumenter() {
+    return INSTRUMENTER;
+  }
+
+  private ApacheHttpClientSingletons() {}
+}
