ac39b07da81e98e522a43df7ff92ff9c5c9e8478
Ken Finnigan <ken@kenfinnigan.me>
2023-09-21 15:00:25 -0400

    feat: Kafka Client payload instrumentation

---
+++ instrumentation/kafka/kafka-clients/kafka-clients-0.11/javaagent/src/main/java/io/lumigo/javaagent/instrumentation/kafkaclients/v0_11/KafkaClientConfigCustomizer.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Lumigo LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package io.lumigo.javaagent.instrumentation.kafkaclients.v0_11;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import java.util.HashMap;
+import java.util.Map;
+
+@AutoService(AutoConfigurationCustomizerProvider.class)
+public class KafkaClientConfigCustomizer implements AutoConfigurationCustomizerProvider {
+  private static final String KAFKA_EXPERIMENTAL_ATTRIBUTES_KEY =
+      "otel.instrumentation.kafka.experimental-span-attributes";
+
+  private static final String KAFKA_PRODUCER_PROPAGATION_ATTRIBUTE_KEY =
+      "otel.instrumentation.kafka.producer-propagation.enabled";
+
+  private static final String MESSAGING_EXPERIMENTAL_RECEIVE_TELEMETRY_KEY =
+      "otel.instrumentation.messaging.experimental.receive-telemetry.enabled";
+
+  @Override
+  public void customize(AutoConfigurationCustomizer autoConfiguration) {
+    autoConfiguration.addPropertiesCustomizer(
+        config -> {
+          Map<String, String> overrides = new HashMap<>();
+
+          // disable OTeL instrumentation for Kafka Clients
+          overrides.put("otel.instrumentation.kafka-clients.enabled", "false");
+
+          // If not set by user, set to true
+          if (null == config.getBoolean(KAFKA_EXPERIMENTAL_ATTRIBUTES_KEY)) {
+            overrides.put(KAFKA_EXPERIMENTAL_ATTRIBUTES_KEY, "true");
+          }
+          if (null == config.getBoolean(KAFKA_PRODUCER_PROPAGATION_ATTRIBUTE_KEY)) {
+            overrides.put(KAFKA_PRODUCER_PROPAGATION_ATTRIBUTE_KEY, "true");
+          }
+          if (null == config.getBoolean(MESSAGING_EXPERIMENTAL_RECEIVE_TELEMETRY_KEY)) {
+            overrides.put(MESSAGING_EXPERIMENTAL_RECEIVE_TELEMETRY_KEY, "true");
+          }
+
+          return overrides;
+        });
+  }
+}
