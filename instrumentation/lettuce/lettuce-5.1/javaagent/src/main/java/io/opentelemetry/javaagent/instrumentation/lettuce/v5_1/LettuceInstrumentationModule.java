8dc63c7a7855e604309d43282b3430ade33fd2ad
Alex Boldyrev <alexanderb@lumigo.com>
2024-08-27 11:52:39 +0400

    2 classes are back

--- instrumentation/lettuce/lettuce-5.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/LettuceInstrumentationModule.java
+++ instrumentation/lettuce/lettuce-5.1/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/lettuce/v5_1/LettuceInstrumentationModule.java
@@ -1,24 +1,36 @@
 /*
- * Copyright The OpenTelemetry Authors
+ * Copyright 2024 Lumigo LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package io.opentelemetry.javaagent.instrumentation.lettuce.v5_1;
-
-import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
-import static java.util.Arrays.asList;
 
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
-import java.util.List;
 import net.bytebuddy.matcher.ElementMatcher;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 
 @AutoService(InstrumentationModule.class)
 public class LettuceInstrumentationModule extends InstrumentationModule {
 
   public LettuceInstrumentationModule() {
-    super("lettuce", "lettuce-5.1");
+    super("lumigo-lettuce", "lumigo-lettuce-5.1");
   }
 
   @Override
@@ -33,7 +45,8 @@ public class LettuceInstrumentationModule extends InstrumentationModule {
 
   @Override
   public List<TypeInstrumentation> typeInstrumentations() {
-    return asList(
-        new DefaultClientResourcesInstrumentation(), new LettuceAsyncCommandInstrumentation());
+    return Arrays.asList(
+        new DefaultClientResourcesInstrumentation(),
+        new LettuceAsyncCommandInstrumentation());
   }
 }
