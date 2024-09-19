6f358cc4ee4ce12c747c7418528c8295d3d04537
Moshe Shaham <moshes@lumigo.io>
2024-09-19 09:29:18 +0300

    RD-13630-add-enable-logs-env-var-for-java-distro

--- smoke-tests/src/test/java/io/lumigo/javaagent/smoketest/EnvironmentVariableSmokeTest.java
+++ smoke-tests/src/test/java/io/lumigo/javaagent/smoketest/EnvironmentVariableSmokeTest.java
@@ -281,19 +281,4 @@ class EnvironmentVariableSmokeTest {
         client,
         (traceNodes) -> assertThat(getProcessEnvironment(traceNodes), is(SCRUBBED_VALUE)));
   }
-
-  @Test
-  public void testLumigoEnableLogs(
-      final @Configuration(
-              env = {
-                @EnvVar(key = "LUMIGO_ENABLE_LOGS", value = "true"),
-              }) TestAppExtension.TestApplication target,
-      final OkHttpClient client)
-      throws IOException {
-    executeTest(
-        target,
-        client,
-        (traceNodes) ->
-            assertThat(target.getLogs(), containsString("Lumigo - enabling logs exporter")));
-  }
 }
