4996fe6e847b72879f67aca85e7746bcda0957e1
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-10-28 14:03:05 +0100

    feat: spotless

--- instrumentation/servlet/servlet-3.0/javaagent/src/test/java/io/lumigo/javaagent/instrumentation/servlet/v3_0/TestServlets.java
+++ instrumentation/servlet/servlet-3.0/javaagent/src/test/java/io/lumigo/javaagent/instrumentation/servlet/v3_0/TestServlets.java
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-class   TestServlets {
+class TestServlets {
   @WebServlet
   public static class EchoStream_single_byte_print extends HttpServlet {
     @Override
