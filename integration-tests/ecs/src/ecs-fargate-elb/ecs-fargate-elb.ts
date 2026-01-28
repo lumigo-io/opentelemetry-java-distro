f582e704a98b8a906d03df65fcbba5b92d7a1597
Eugene Orlovsky <eugeneo@lumigo.io>
2026-01-28 21:02:39 +0100

    feat: OpenTelemetry upgrade from 1.32.0 to 1.44.1/2.10.0

--- integration-tests/ecs/src/ecs-fargate-elb/ecs-fargate-elb.ts
+++ integration-tests/ecs/src/ecs-fargate-elb/ecs-fargate-elb.ts
@@ -82,7 +82,10 @@ export class EcsFargateElbStack extends Stack {
       environment: {
         AWS_REGION: props.env?.region || 'unknown_region',
         SERVER_PORT: String(serverPort),
-        LUMIGO_DEBUG_SPANDUMP: '/dev/stdout'
+        LUMIGO_DEBUG_SPANDUMP: '/dev/stdout',
+        LUMIGO_DEBUG: 'true',
+        OTEL_JAVAAGENT_DEBUG: 'true',
+        OTEL_LOG_LEVEL: 'debug'
       },
       secrets: {
         LUMIGO_TRACER_TOKEN: lumigoTokenSecret,
